c745c38e6a0de62b7159abcd76f0335cf3f1d0a0
Virgil Sealy <virgil.sealy@gmail.com>
2018-06-09 22:18:01 -0400

    Add project files

---
+++ Assets/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    public float slowDownFactor = 10;
+
+	public void EndGame () {
+        StartCoroutine(RestartLevel());
+	}
+
+    IEnumerator RestartLevel()
+    {
+        Time.timeScale = 1f / slowDownFactor;
+        Time.fixedDeltaTime = Time.fixedDeltaTime / slowDownFactor;
+
+        yield return new WaitForSeconds(1f / slowDownFactor);
+
+        Time.timeScale = 1f;
+        Time.fixedDeltaTime = Time.fixedDeltaTime * slowDownFactor;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public Transform playArea;
+    public float speed = 15f;
+
+    public float mapWidth = 5f;
+
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        mapWidth = playArea.transform.localScale.x / 2f;
+    }
+    private void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal") * Time.fixedDeltaTime * speed;
+        Vector2 newPos = rb.position + Vector2.right * x;
+        newPos.x = Mathf.Clamp(newPos.x, -mapWidth, mapWidth);
+        rb.MovePosition(newPos);
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        FindObjectOfType<GameManager>().EndGame();
+    }
+}
